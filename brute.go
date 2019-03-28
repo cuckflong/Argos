88afe7912d7e473298bc7125d6e899cd3e63b1a9
johnathanclong <johnathanliauw@yahoo.com.hk>
2019-03-28 23:51:18 +1100

    will do multi-thread and optimization later lmao

--- brute.go
+++ brute.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
 
 	"github.com/fatih/color"
 	"github.com/go-ini/ini"
@@ -14,15 +19,34 @@ import (
 const SUCCESS = 0
 const PROGRESS = 1
 const ERROR = 2
-const GET = 3
-const POST = 4
 
-var url string
-var query string
+type header struct {
+	name  string
+	value string
+}
+
+type cookie struct {
+	name  string
+	value string
+}
+
+type data struct {
+	name  string
+	value string
+}
+
+var headers []header
+var cookies []cookie
+var dataList []data
+var client *http.Client
+var result bytes.Buffer
+var urlPath string
 var charFile string
 var identifier string = "ARGOS"
+var valid string
+var invalid string
 var config string
-var method int
+var method string
 
 var charList = []string{
 	"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
@@ -47,6 +71,7 @@ func init() {
 	flag.StringVar(&config, "config", "", "Config file")
 	flag.StringVar(&charFile, "list", "", "The file for character list")
 	flag.Parse()
+
 	if config == "" {
 		printStatus("Config file not provided", ERROR)
 		os.Exit(1)
@@ -56,11 +81,32 @@ func init() {
 		printStatus("Config file not found", ERROR)
 		os.Exit(1)
 	}
-	url = cfg.Section("request").Key("url").String()
-	method = cfg.Section("request").Key("method").String()
+	urlPath = cfg.Section("basic").Key("url").String()
+	method = cfg.Section("basic").Key("method").String()
+	valid = cfg.Section("response").Key("valid").String()
+	invalid = cfg.Section("response").Key("invalid").String()
+	if urlPath == "" || method == "" {
+		printStatus("Please provide URL and request method", ERROR)
+		os.Exit(1)
+	}
+	if valid == "" && invalid == "" {
+		printStatus("Please provide valid or invalid response", ERROR)
+		os.Exit(1)
+	}
+	for _, key := range cfg.Section("headers").KeyStrings() {
+		hd := header{name: key, value: cfg.Section("headers").Key(key).String()}
+		headers = append(headers, hd)
+	}
+	for _, key := range cfg.Section("cookies").KeyStrings() {
+		ck := cookie{name: key, value: cfg.Section("cookies").Key(key).String()}
+		cookies = append(cookies, ck)
+	}
+	for _, key := range cfg.Section("data").KeyStrings() {
+		dt := data{name: key, value: cfg.Section("data").Key(key).String()}
+		dataList = append(dataList, dt)
+	}
 
-	keys := cfg.Section("cookies").Keys()
-	fmt.Println(keys)
+	getCharList()
 }
 
 func printStatus(msg string, status int) {
@@ -68,7 +114,7 @@ func printStatus(msg string, status int) {
 	case SUCCESS:
 		color.Green("[+] %s", msg)
 	case PROGRESS:
-		color.Blue("[-] %s", msg)
+		color.Yellow("[-] %s", msg)
 	case ERROR:
 		color.Red("[!] %s", msg)
 	}
@@ -76,6 +122,7 @@ func printStatus(msg string, status int) {
 
 func getCharList() {
 	if charFile == "" {
+		printStatus("Character list not provided, using default list", PROGRESS)
 		return
 	}
 	file, err := os.Open(charFile)
@@ -90,18 +137,121 @@ func getCharList() {
 	defer file.Close()
 }
 
-func getRequest() {
-	//req, err := http.NewRequest("GET", url, nil)
-	//q := req.URL.Query()
-	return
+func getRequest(payload string) bool {
+	req, err := http.NewRequest("GET", urlPath, nil)
+	if err != nil {
+		printStatus("Error creating GET request", ERROR)
+		os.Exit(1)
+	}
+	for _, hd := range headers {
+		new_name := strings.Replace(hd.name, identifier, payload, 1)
+		new_value := strings.Replace(hd.value, identifier, payload, 1)
+		req.Header.Add(new_name, new_value)
+	}
+	for _, ck := range cookies {
+		new_name := strings.Replace(ck.name, identifier, payload, 1)
+		new_value := strings.Replace(ck.value, identifier, payload, 1)
+		c := http.Cookie{Name: new_name, Value: new_value}
+		req.AddCookie(&c)
+	}
+	q := req.URL.Query()
+	for _, dt := range dataList {
+		new_name := strings.Replace(dt.name, identifier, payload, 1)
+		new_value := strings.Replace(dt.value, identifier, payload, 1)
+		q.Add(new_name, new_value)
+	}
+	req.URL.RawQuery = q.Encode()
+	resp, err := client.Do(req)
+	if err != nil {
+		printStatus("Error performing GET request", ERROR)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if strings.Contains(string(respBody), valid) && !(invalid != "" && strings.Contains(string(respBody), invalid)) {
+		return true
+	}
+	return false
 }
 
-func postRequest() {
-	//req, err := http.NewRequest("POST", url, nil)
-
-	return
+func postRequest(payload string) bool {
+	var body bytes.Buffer
+	for index, dt := range dataList {
+		new_name := strings.Replace(dt.name, identifier, payload, 1)
+		new_value := strings.Replace(dt.value, identifier, payload, 1)
+		if index != 0 {
+			body.WriteString("&")
+		}
+		body.WriteString(new_name + "=" + new_value)
+	}
+	req, err := http.NewRequest("POST", urlPath, bytes.NewBuffer(body.Bytes()))
+	if err != nil {
+		printStatus("Error creating POST request", ERROR)
+		os.Exit(1)
+	}
+	for _, hd := range headers {
+		new_name := strings.Replace(hd.name, identifier, payload, 1)
+		new_value := strings.Replace(hd.value, identifier, payload, 1)
+		req.Header.Add(new_name, new_value)
+	}
+	for _, ck := range cookies {
+		new_name := strings.Replace(ck.name, identifier, payload, 1)
+		new_value := strings.Replace(ck.value, identifier, payload, 1)
+		c := http.Cookie{Name: new_name, Value: new_value}
+		req.AddCookie(&c)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		printStatus("Error performing POST request", ERROR)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if strings.Contains(string(respBody), valid) && !(invalid != "" && strings.Contains(string(respBody), invalid)) {
+		return true
+	}
+	return false
 }
 
 func main() {
-
+	var proxyStr string = "http://localhost:8080"
+	proxyURL, _ := url.Parse(proxyStr)
+	transport := &http.Transport{
+		Proxy: http.ProxyURL(proxyURL),
+	}
+	client = &http.Client{
+		Transport: transport,
+	}
+	var found bool
+	for {
+		found = false
+		for _, chr := range charList {
+			switch method {
+			case "get":
+				if getRequest(result.String() + chr) {
+					result.WriteString(chr)
+					found = true
+				}
+			case "post":
+				if postRequest(result.String() + chr) {
+					result.WriteString(chr)
+					found = true
+				}
+			}
+			if found {
+				printStatus(result.String(), SUCCESS)
+				break
+			}
+		}
+		if !found {
+			if result.String() == "" {
+				printStatus("No result found", ERROR)
+				os.Exit(1)
+			} else {
+				printStatus("Result found", SUCCESS)
+				printStatus(result.String(), SUCCESS)
+				os.Exit(0)
+			}
+		}
+	}
 }
