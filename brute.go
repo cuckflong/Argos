0d5807f2c2a4e9619160f71e35fc7ca0e8eb84ce
johnathanclong <johnathanliauw@yahoo.com.hk>
2019-03-28 17:46:35 +1100

    lol

--- brute.go
+++ brute.go
@@ -5,25 +5,30 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
+
+	"github.com/fatih/color"
+	"github.com/go-ini/ini"
 )
 
 const SUCCESS = 0
 const PROGRESS = 1
 const ERROR = 2
+const GET = 3
+const POST = 4
 
 var url string
 var query string
 var charFile string
 var identifier string = "ARGOS"
-var post bool
+var config string
+var method int
 
 var charList = []string{
 	"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
 	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
 	"1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
-	" ", "{", "}",
+	" ", "{", "}", "#", "$", "!", "@",
 }
 
 func banner() {
@@ -39,17 +44,33 @@ func banner() {
 
 func init() {
 	banner()
-	flag.StringVar(&url, "url", "", "The target URL or Domain")
-	flag.StringVar(&query, "query", "", "The query for blind SQL injection")
+	flag.StringVar(&config, "config", "", "Config file")
 	flag.StringVar(&charFile, "list", "", "The file for character list")
-	flag.BoolVar(&post, "post", false, "Use POST request")
 	flag.Parse()
+	if config == "" {
+		printStatus("Config file not provided", ERROR)
+		os.Exit(1)
+	}
+	cfg, err := ini.Load(config)
+	if err != nil {
+		printStatus("Config file not found", ERROR)
+		os.Exit(1)
+	}
+	url = cfg.Section("request").Key("url").String()
+	method = cfg.Section("request").Key("method").String()
+
+	keys := cfg.Section("cookies").Keys()
+	fmt.Println(keys)
 }
 
 func printStatus(msg string, status int) {
 	switch status {
 	case SUCCESS:
-
+		color.Green("[+] %s", msg)
+	case PROGRESS:
+		color.Blue("[-] %s", msg)
+	case ERROR:
+		color.Red("[!] %s", msg)
 	}
 }
 
@@ -70,14 +91,14 @@ func getCharList() {
 }
 
 func getRequest() {
-	resp, err := http.Get(url)
-	if err != nil {
-
-	}
+	//req, err := http.NewRequest("GET", url, nil)
+	//q := req.URL.Query()
 	return
 }
 
 func postRequest() {
+	//req, err := http.NewRequest("POST", url, nil)
+
 	return
 }
 
